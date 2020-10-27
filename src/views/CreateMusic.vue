8bc343a6648fd08c38a6f181b3b2ef9ed83abff4
change <och8808@gmail.com>
2020-10-27 15:50:43 +0900

    update

--- src/views/CreateMusic.vue
+++ src/views/CreateMusic.vue
@@ -5,24 +5,24 @@
       <div class="input-group-prepend">
         <span class="input-group-text" id="singer">singer</span>
       </div>
-      <input type="text" class="form-control" placeholder="BTS, BlackPink">
+      <input type="text" class="form-control" placeholder="BTS, BlackPink" v-model="singer">
     </div>
 
     <div class="input-group mb-3">
       <div class="input-group-prepend">
         <span class="input-group-text" id="title">title</span>
       </div>
-      <input type="text" class="form-control" placeholder="dynamite, 마지막처럼" >
+      <input type="text" class="form-control" placeholder="dynamite, 마지막처럼" v-model="title">
     </div>
 
     <div class="input-group mb-3">
       <div class="input-group-prepend">
         <span class="input-group-text" id="basic-addon3">https://youtu.be/</span>
       </div>
-      <input type="text" class="form-control" id="basic-url" >
+      <input type="text" class="form-control" id="basic-url" v-model="youtube_url">
     </div>
 
-    <button type="button" class="btn btn-primary btn-lg btn-block">submit</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block" @click="createMusic">submit</button>
 
   </div>
 
@@ -34,11 +34,41 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'Home',
+  name: 'CreateMusic',
+  data () {
+    return {
+      'singer': '',
+      'title': '',
+      'youtube_url': '',
+    }
+  },
   components: {
     // HelloWorld
+  },
+  methods: {
+    createMusic: function () {
+      const data = {
+        singer: this.singer,
+        title: this.title,
+        youtube_url: this.youtube_url
+      }
+      axios.post('http://127.0.0.1:8000/musics/', data)
+        .then(res => {
+          console.log(res)
+          if (res.status == 200) {
+            alert('등록완료')
+            this.singer = ''
+            this.title = ''
+            this.youtube_url = ''
+          }
+          else {
+            alert('실패')
+          }
+        })
+    }
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>CatchMusic</h1>
+    <button type="button" class="btn btn-primary" @click="getRandomMusic">Random</button>
+    <hr>
+    <iframe :src="videoUrl" frameborder="0" class="iframe" :class="{ none: !iframeBool, block: iframeBool}"></iframe>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      videoId: '',
+      iframeBool: false,
+    }
+  },
+  components: {
+    // HelloWorld
+  },
+  methods: {
+    getRandomMusic: function () {
+      this.iframeBool = true
+      axios.get('http://127.0.0.1:8000/musics/random/')
+        .then(res => {
+          console.log(res.data.youtube_url)
+          this.videoId = res.data.youtube_url
+        })
+    }
+  },
+  computed: {
+    videoUrl () {
+      const videoId = this.videoId;
+      return `https://www.youtube.com/embed/${videoId}?autoplay=1`
+    }
+  },
+  // mounted() {
+  //   videoUrl()
+  // }
+}
+</script>
+
+<style scoped>
+  .iframe {
+
+  }
+  .none {
+    display: none;
+  }
+  .block {
+    display: inline;
+  }
+</style>
